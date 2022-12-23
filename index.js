3142287e210ec82115f935ac48572f72fb69e9c0
inzamam nur <inzamamnur.14@gmail.com>
2022-12-23 19:56:23 +0600

    updated

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const express = require("express");
 const app = express();
 const port = process.env.Port || 5000;
-
 const cors = require("cors");
+require("dotenv").config();
+const { mongoose, ObjectId } = require("mongodb");
 
 app.use(cors());
-const course = require("./Data/course.json");
-const coursesname = require("./Data/coursesname.json");
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nnocokg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usercollection = client.db("job_task").collection("user_info");
+
+    app.get("/userinfo", async (req, res) => {
+      const query = {};
+      const cursor = usercollection.find(query);
+      const userinfo = await cursor.toArray();
+      res.send(userinfo);
+    });
+
+    app.post("/userinfo", async (req, res) => {
+      const userinfo = req.body;
+      const result = await usercollection.insertOne(userinfo);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Api Running");
 });
 
-
-
-
-
 app.listen(port, () => {
-  console.log("running", port);
+  console.log("running api", port);
 });
